b87b785d4fb1d92fa68d71b5c17046d04373e28e
Charles Cognato <charlescognato@gmail.com>
2025-09-30 19:55:22 -0400

    Add app title constant.

--- tkinter_lab/tkinter_app.py
+++ tkinter_lab/tkinter_app.py
@@ -2,11 +2,12 @@ import tkinter as tk
 
 from mainwindow import MainWindow
 
+APP_TITLE = "CopyDocs"
 WINDOW_X_DIMENSION = 800
 WINDOW_Y_DIMENSION = 500
 
 root = tk.Tk()
-root.title("My Tkinter App")
+root.title(f"{APP_TITLE}")
 root.geometry(f"{WINDOW_X_DIMENSION}x{WINDOW_Y_DIMENSION}")
 root.option_add("*tearOff", tk.FALSE)
 
